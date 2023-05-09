bcd3259baed38ff63cff02917cd99d4315979fc4
Damien Ruban <sc19dr@leeds.ac.uk>
2023-05-09 18:10:11 +0100

    Changing views, viewset unnecessary

--- flightrest/src/views.py
+++ flightrest/src/views.py
@@ -1,21 +1,6 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+import json
+import datetime
 from src.models import Flight, Seat, Reservation, Passenger
 from src.serializers import FlightSerializer, SeatSerializer, ReservationSerializer, PassengerSerializer
-
-class FlightViewSet(viewsets.ModelViewSet):
-    query = Flight.objects.all()
-    serializer_class = FlightSerializer
-
-class SeatViewSet(viewsets.ModelViewSet):
-    query = Seat.objects.all()
-    serializer_class = SeatSerializer
-
-class ReservationViewSet(viewsets.ModelViewSet):
-    query = Reservation.objects.all()
-    serializer_class = ReservationSerializer
-
-class PassengerViewSet(viewsets.ModelViewSet):
-    query = Passenger.objects.all()
-    serializer_class = PassengerSerializer
