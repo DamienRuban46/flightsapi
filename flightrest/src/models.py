e13e189929c3a836c20d6542b02b00d2cdf74594
Damien Ruban <sc19dr@leeds.ac.uk>
2023-05-10 15:57:44 +0100

    Changes to db

--- flightrest/src/models.py
+++ flightrest/src/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Flight(models.Model):
-    flight_id = models.UUIDField(primary_key=True)
+    flight_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     plane_model = models.CharField(max_length=100)
     number_of_rows = models.IntegerField()
     seats_per_row = models.SmallIntegerField()
@@ -12,13 +13,14 @@ class Flight(models.Model):
     origin = models.CharField(max_length=100)
 
 class Seat(models.Model):
-    seat_id = models.UUIDField(primary_key=True)
-    flight_id = models.ForeignKey(Flight, on_delete=models.DO_NOTHING)
+    seat_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    flight_id = models.ForeignKey(Flight, on_delete=models.CASCADE)
     seat_number = models.IntegerField()
     seat_price = models.FloatField()
+    booked = models.BooleanField()
 
 class Passenger(models.Model):
-    passenger_id = models.IntegerField()
+    passenger_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     DOB = models.DateField()
@@ -26,7 +28,7 @@ class Passenger(models.Model):
     address = models.CharField(max_length=100)
 
 class Reservation(models.Model):
-    reservation = models.IntegerField(primary_key=True)
+    reservation = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     seat_id = models.ForeignKey(Seat, on_delete=models.DO_NOTHING)
     passenger_id = models.ForeignKey(Passenger, on_delete=models.DO_NOTHING)
     hold_luggage = models.BooleanField()
